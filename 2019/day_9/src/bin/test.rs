3eb7c585eefacd97bf8f63a2e94411406d0e1c7a
haleywm <haleywrkmn@gmail.com>
2020-12-08 13:17:44 +0800

    Completed day 9 2019

---
+++ 2019/day_9/src/bin/test.rs
@@ -0,0 +1,13 @@
+use day_9::std_intcode;
+use std::fs;
+use std::io::{self, BufRead};
+
+fn main() {
+    let file = fs::File::open("input.txt").expect("Invalid Filename");
+    let mut line = String::new();
+    io::BufReader::new(file)
+        .read_line(&mut line)
+        .expect("Couldn't read file");
+
+    std_intcode(line.trim()).join().unwrap();
+}

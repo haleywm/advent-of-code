c270a2c907ea9e6c0f0f0404cacac60626722096
haleywm <haleywrkmn@gmail.com>
2020-12-10 15:27:44 +0800

    Finished

--- 2019/day_10/src/bin/problem_two.rs
+++ 2019/day_10/src/bin/problem_two.rs
@@ -27,12 +27,14 @@ fn main() {
     
     let points = get_visible_by_all_points(&met_map);
     let high = points.iter().max_by_key(|x| x.0).unwrap();
+    let point = high.1;
+    //let point = (3, 8);
     println!("{}: ({}, {})", high.0, high.1.0, high.1.1);
     // After getting the optimal position, getting meteor destruction positions
     let mut destroyed = 0;
-    const TARGET: usize = 9;
+    const TARGET: usize = 200;
     loop {
-        let mut to_destroy = get_visible_by_point(&met_map, high.1);
+        let mut to_destroy = get_visible_by_point(&met_map, point);
         if destroyed + to_destroy.len() < TARGET {
             // Order doesn't matter since a full rotation won't do it
             destroyed += to_destroy.len();
@@ -42,10 +44,12 @@ fn main() {
         }
         else {
             // Sorting the last ones to find the exact one
-            to_destroy.sort_unstable_by(|x, y| (x.0 as f32).atan2(-(x.1 as f32)).partial_cmp(&(y.0 as f32).atan2(-(y.1 as f32))).unwrap_or(Equal));
+            to_destroy.sort_unstable_by(|x, y| (y.1 as f32 - point.1 as f32).atan2((y.0 as f32 - point.0 as f32)).partial_cmp(&(x.1 as f32 - point.1 as f32).atan2((x.0 as f32 - point.0 as f32))).unwrap_or(Equal));
+            //to_destroy.sort_by(|x, y| x.1.cmp(&y.1));
             let pos = TARGET - destroyed - 1;
             let item = to_destroy[pos];
-            println!("{}, {}: {}", item.0, item.1, item.0 * 100 + item.1);
+            //println!("{:?}", to_destroy);
+            println!("{}, {}: {}", item.0, item.1, item.1 * 100 + item.0);
             break;
         }
 
@@ -54,16 +58,16 @@ fn main() {
 
 fn get_visible_by_all_points(map: &Vec<Vec<bool>>) -> Vec<(usize, (usize, usize))> {
     let mut result: Vec<(usize, (usize, usize))> = Vec::new();
-    for x in 0..map.len() {
-        'parent_count: for y in 0..map[0].len() {
+    for y in 0..map.len() {
+        'parent_count: for x in 0..map[0].len() {
             // x across, y down
-            if !map[x][y] {
+            if !map[y][x] {
                 // Only checking from positions with meteors
                 continue 'parent_count;
             }
-            let total = get_visible_by_point(map, (x, y)).len();
+            let total = get_visible_by_point(map, (y, x)).len();
             
-            result.push((total, (x, y)));
+            result.push((total, (y, x)));
         }
     }
     result
@@ -71,10 +75,10 @@ fn get_visible_by_all_points(map: &Vec<Vec<bool>>) -> Vec<(usize, (usize, usize)
 
 fn get_visible_by_point(map: &Vec<Vec<bool>>, point: (usize, usize)) -> Vec<(usize, usize)> {
     let mut total = Vec::new();
-    for comp_x in 0..map.len() {
-        'child_test: for comp_y in 0..map[0].len() {
-            let diff = (comp_x as i64 - point.0 as i64, comp_y as i64 - point.1 as i64);
-            if map[comp_x][comp_y] && (diff.0 != 0 || diff.1 != 0) {
+    for comp_y in 0..map.len() {
+        'child_test: for comp_x in 0..map[0].len() {
+            let diff = (comp_y as i64 - point.0 as i64, comp_x as i64 - point.1 as i64);
+            if map[comp_y][comp_x] && (diff.0 != 0 || diff.1 != 0) {
                 let place_count = gcd(diff.0, diff.1);
                 for mult in 1..place_count {
                     // If place_count is greater than 1, check each subspot for meteors
@@ -84,7 +88,7 @@ fn get_visible_by_point(map: &Vec<Vec<bool>>, point: (usize, usize)) -> Vec<(usi
                     }
                 }
                 // Passed tests, this is a meteor
-                total.push((comp_x, comp_y));
+                total.push((comp_y, comp_x));
             }
         }
     }

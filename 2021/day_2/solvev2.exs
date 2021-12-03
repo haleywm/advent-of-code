b8d0a42b1cbb9e6900c923d13d918117bdad14d0
Haley Workman <haleywrkmn@gmail.com>
2021-12-03 10:48:55 +0800

    Did it again but more recursive

---
+++ 2021/day_2/solvev2.exs
@@ -0,0 +1,39 @@
+parser = ~r/(up|down|forward) (\d+)/
+
+instructions =
+  File.read!("input.txt")
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn x ->
+    [_, instr, num] = Regex.run(parser, x)
+    {String.to_atom(instr), String.to_integer(num)}
+  end)
+
+defmodule P1 do
+  def move(list, pos \\ {0, 0})
+  def move([{:forward, dist} | tail], {x, y}), do: move(tail, {x + dist, y})
+  def move([{:up, dist} | tail], {x, y}), do: move(tail, {x, y - dist})
+  def move([{:down, dist} | tail], {x, y}), do: move(tail, {x, y + dist})
+  def move([], {x, y}), do: x * y
+end
+
+defmodule P2 do
+  def move(list, pos \\ {0, 0}, aim \\ 0)
+
+  def move([{:forward, dist} | tail], {x, y}, aim),
+    do: move(tail, {x + dist, y + aim * dist}, aim)
+
+  def move([{:up, dist} | tail], {x, y}, aim),
+    do: move(tail, {x, y}, aim - dist)
+
+  def move([{:down, dist} | tail], {x, y}, aim),
+    do: move(tail, {x, y}, aim + dist)
+
+  def move([], {x, y}, _), do: x * y
+end
+
+# Part 1
+P1.move(instructions)
+|> IO.puts()
+
+P2.move(instructions)
+|> IO.puts()

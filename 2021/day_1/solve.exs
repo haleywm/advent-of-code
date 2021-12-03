7bd1f292b35a1a62bf8187ca062fa3128394f66f
Haley Workman <haleywrkmn@gmail.com>
2021-12-03 10:38:47 +0800

    Formatted elixir

--- 2021/day_1/solve.exs
+++ 2021/day_1/solve.exs
@@ -1,20 +1,21 @@
-lines = File.read!("input.txt")
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.to_integer/1)
+lines =
+  File.read!("input.txt")
+  |> String.split("\n", trim: true)
+  |> Enum.map(&String.to_integer/1)
 
 count_descending = fn x ->
-    Stream.chunk_every(x, 2, 1, :discard)
-        |> Enum.count(fn [prev, cur] -> cur > prev end)
+  Stream.chunk_every(x, 2, 1, :discard)
+  |> Enum.count(fn [prev, cur] -> cur > prev end)
 end
 
 # Task 1
 lines
-    |> count_descending.()
-    |> IO.puts
+|> count_descending.()
+|> IO.puts()
 
 # Task 2
 lines
-    |> Stream.chunk_every(3, 1, :discard)
-    |> Stream.map(&Enum.sum/1)
-    |> count_descending.()
-    |> IO.puts
+|> Stream.chunk_every(3, 1, :discard)
+|> Stream.map(&Enum.sum/1)
+|> count_descending.()
+|> IO.puts()
--- 2021/day_2/solve.exs
+++ 2021/day_2/solve.exs
@@ -1,28 +1,29 @@
 parser = ~r/(up|down|forward) (\d+)/
 
-instructions = File.read!("input.txt")
-    |> String.split("\n", trim: true)
-    |> Stream.map(fn x ->
-        [_, instr, num] = Regex.run(parser, x)
-        {String.to_atom(instr), String.to_integer(num)}
-        end)
-    |> Enum.map(fn
-        {:forward, n} -> {n, 0}
-        {:up, n} -> {0, -n}
-        {:down, n} -> {0, n}
-        end)
+instructions =
+  File.read!("input.txt")
+  |> String.split("\n", trim: true)
+  |> Stream.map(fn x ->
+    [_, instr, num] = Regex.run(parser, x)
+    {String.to_atom(instr), String.to_integer(num)}
+  end)
+  |> Enum.map(fn
+    {:forward, n} -> {n, 0}
+    {:up, n} -> {0, -n}
+    {:down, n} -> {0, n}
+  end)
 
 # Part 1
 instructions
-    |> Enum.reduce(fn {x, y}, {curx, cury} -> {curx + x, cury + y} end)
-    |> Tuple.product
-    |> IO.puts
+|> Enum.reduce(fn {x, y}, {curx, cury} -> {curx + x, cury + y} end)
+|> Tuple.product()
+|> IO.puts()
 
 # Part 2
 instructions
-    |> Enum.reduce({0, 0, 0}, fn {move, adjust}, {curx, cury, aim} ->
-        aim = aim + adjust
-        {curx + move, cury + move * aim, aim}
-        end)
-    |> then(fn {x, y, _} -> x * y end)
-    |> IO.puts
+|> Enum.reduce({0, 0, 0}, fn {move, adjust}, {curx, cury, aim} ->
+  aim = aim + adjust
+  {curx + move, cury + move * aim, aim}
+end)
+|> then(fn {x, y, _} -> x * y end)
+|> IO.puts()

46489fb238dee686c8728e1f0c0183a90dee49d2
Haley Workman <haleywrkmn@gmail.com>
2021-12-05 10:55:55 +0800

    Mostly done part 1, need bugfixing

---
+++ 2021/day_4/solve.exs
@@ -0,0 +1,85 @@
+defmodule Bingo do
+  @moduledoc """
+  Provides a set of functions useful for managing bingo games
+  """
+  defstruct all_numbers: MapSet.new(), winning_sets: []
+  @doc """
+  Takes an enumerator of strings and turns them into a bingo board
+  """
+  def parse_board(lines) do
+    rows = Enum.map(lines, &Bingo.to_numbers(&1, " "))
+    last_index = length(rows) - 1
+    all_points = 0..last_index
+
+    columns = for i <- all_points do
+      for j <- all_points, into: %MapSet{}, do: Enum.at(rows, j) |> Enum.at(i)
+    end
+
+    row_sets = Stream.map(rows, &MapSet.new/1)
+
+    # Using a list instead as I can't find a guarantee that all numbers are unique
+    #all_numbers = MapSet.new(Stream.concat(rows))
+    all_numbers = Enum.concat(rows)
+
+    %Bingo{all_numbers: all_numbers, winning_sets: Enum.concat([row_sets, columns])}
+  end
+
+  @doc """
+  Takes a string containing a list of numbers, and a value to split the string by, and then splits the string and its contents
+  """
+  def to_numbers(string, splitter) do
+    String.split(string, splitter, trim: true)
+    |> Stream.map(&String.trim/1)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @doc """
+  Takes a list of boards, and a list of numbers to draw, in order, and then simualates a game. Returns the score of the winner.
+  """
+  def run_game(boards, numbers) do
+    # Iterating through the numbers, building a set, and seeing if it was a winner for each game
+    Enum.reduce_while(numbers, MapSet.new(), fn next, acc ->
+      selected = MapSet.put(acc, next)
+      winner = Enum.find(boards, fn board ->
+        Enum.any?(board.winning_sets, fn set ->
+          MapSet.subset?(set, selected)
+        end)
+      end)
+      # Checking if a winner or not
+      if is_nil(winner) do
+        # No winner
+        {:cont, selected}
+      else
+        # Winner!
+        {:halt, get_points(winner, numbers) * next}
+      end
+    end)
+  end
+
+  @doc """
+  Returns the number of points a board would have won, by adding all numbers that aren't part of numbers
+  """
+  def get_points(board, numbers) do
+    Enum.sum(board.all_numbers -- Enum.to_list(numbers))
+  end
+end
+
+numbers =
+  File.stream!("input.txt")
+
+to_select =
+Enum.at(numbers, 0)
+|> then(&Bingo.to_numbers(&1, ","))
+
+#IO.inspect(to_select)
+
+boards =
+numbers
+|> Stream.drop(2)
+|> Stream.chunk_every(5, 6, :discard)
+|> Enum.map(&Bingo.parse_board/1)
+
+#IO.inspect(boards)
+
+Bingo.run_game(boards, to_select)
+|> IO.puts

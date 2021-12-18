f1bdd373a9a096f94bb79065896876f773139c43
Haley Workman <haleywrkmn@gmail.com>
2021-12-18 21:56:34 +0800

    Done day 15

--- 2021/rest/src/bin/day_15.rs
+++ 2021/rest/src/bin/day_15.rs
@@ -1,6 +1,27 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::fs;
 use std::io::{self, BufRead};
+use std::iter;
+
+#[derive(Eq, PartialEq, Clone)]
+struct MapNode {
+    pos: (usize, usize),
+    distance: usize,
+}
+
+impl Ord for MapNode {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.distance.cmp(&other.distance).reverse()
+    }
+}
+
+impl PartialOrd for MapNode {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 fn main() {
     let file = fs::File::open("input/day_15.txt").expect("Invalid Filename");
@@ -14,7 +35,7 @@ fn main() {
                 .collect()
         })
         .collect();
-    
+
     // Part 1
     println!("{}", cheapest_path(&grid));
 
@@ -22,7 +43,7 @@ fn main() {
     let grid_size = grid.len();
     let big_grid_size = grid_size * 5;
     let mut big_grid: Vec<Vec<u32>> = vec![Vec::with_capacity(big_grid_size); big_grid_size];
-    
+
     for x in 0..big_grid_size {
         for y in 0..big_grid_size {
             let orig = grid[x % grid_size][y % grid_size] - 1;
@@ -33,67 +54,87 @@ fn main() {
     println!("{}", cheapest_path(&big_grid));
 }
 
-fn cheapest_path(grid: &Vec<Vec<u32>>) -> u32 {
+fn cheapest_path(grid: &[Vec<u32>]) -> u32 {
     let grid_size = grid.len();
-    
-    // Implementing dijkstras algorithm
-    // Creating a map containing all unvisited nodes
-    let mut unvisited: HashMap<(usize, usize), Option<u32>> = HashMap::with_capacity(grid_size * grid_size);
-    // And a map for all visited nodes
-    let mut visited: HashMap<(usize, usize), u32> = HashMap::with_capacity(grid_size * grid_size);
 
-    // Inserting all nodes into unvisited
-    unvisited.insert((0, 0), Some(0));
+    // Implementing A* algorithm
+    // The heiruestic algorithm will be a simple hanhattan distance to the end
+    // As if it could take a direct path of 1's there that would be the distance
+    // Creating a map containing visitable nodes
+    let mut open_nodes: BinaryHeap<MapNode> = BinaryHeap::new();
+    open_nodes.push(MapNode {
+        pos: (0, 0),
+        distance: grid_size * 2 - 2,
+    });
+
+    let mut lowest_dist: HashMap<(usize, usize), Option<u32>> =
+        HashMap::with_capacity(grid_size * grid_size);
+    lowest_dist.insert((0, 0), Some(0));
     for x in 0..grid_size {
         for y in 0..grid_size {
-            if x == 0 && y == 0 {
-                continue;
+            if x != 0 || y != 0 {
+                lowest_dist.insert((x, y), None);
             }
-            unvisited.insert((x, y), None);
         }
     }
 
-    let mut cur: (usize, usize);
     let goal = (grid_size - 1, grid_size - 1);
-    while !visited.contains_key(&goal) {
-        // Now selecting the next cur
-        cur = *unvisited
-            .iter()
-            .filter(|(_, val)| val.is_some())
-            .min_by_key(|(_, val)| val.unwrap())
-            //.expect("Ran out of nodes to visit")
-            .unwrap_or_else(|| panic!("Panicked with {} visited, {} unvisited: {:?}", visited.len(), unvisited.len(), visited))
-            .0;
-        let cur_cost = unvisited.get(&cur).unwrap().unwrap();
-        for neighbour in get_unvisited_neighbours(cur, &visited, grid_size - 1) {
-            let comp = unvisited.get_mut(&neighbour).unwrap();
-            let n_cost = grid[neighbour.0][neighbour.1];
-            // If the current value is None, or if it's greater than the current cost plus the travel cost, update
-            if comp.map_or(true, |n_val| n_val > cur_cost + n_cost) {
-                *comp = Some(cur_cost + n_cost);
+
+    while let Some(cur) = open_nodes.pop() {
+        if cur.pos == goal {
+            return lowest_dist.get(&cur.pos).unwrap().unwrap();
+        }
+
+        for neighbour in get_neighbours(cur.pos, grid_size - 1) {
+            let travel_score =
+                lowest_dist.get(&cur.pos).unwrap().unwrap() + grid[neighbour.0][neighbour.1];
+            // If there isn't currently a defined lowest distance, or if the defined lowest distance is greater than the trip from cur to neighbour, set that
+            if lowest_dist
+                .get(&neighbour)
+                .unwrap()
+                .map_or(true, |cur_travel| cur_travel > travel_score)
+            {
+                lowest_dist.insert(neighbour, Some(travel_score));
+                // If neighbour isn't in open_nodes, add it
+                let neighbour_node = open_nodes.iter().find(|node| node.pos == neighbour);
+                let new_node = MapNode {
+                    pos: neighbour,
+                    distance: travel_score as usize + grid_size * 2 - neighbour.0 - neighbour.1 - 2,
+                };
+                match neighbour_node {
+                    None => open_nodes.push(new_node),
+                    Some(node) => {
+                        if new_node > *node {
+                            let node = node.clone();
+                            // Removing the old one and adding the new one
+                            open_nodes = open_nodes
+                                .drain()
+                                .filter(|x| node != *x)
+                                .chain(iter::once(new_node))
+                                .collect();
+                        }
+                    }
+                }
             }
         }
-        // Now removing cur from unvisited as it's been visited
-        unvisited.remove(&cur);
-        visited.insert(cur, cur_cost);
     }
 
-    *visited.get(&goal).unwrap()
+    panic!("Failed to find path");
 }
 
-fn get_unvisited_neighbours(cur: (usize, usize), visited: &HashMap<(usize, usize), u32>, max: usize) -> Vec<(usize, usize)> {
+fn get_neighbours(cur: (usize, usize), max: usize) -> Vec<(usize, usize)> {
     let mut neighbours = Vec::with_capacity(4);
 
-    if cur.0 > 0 && !visited.contains_key(&(cur.0 - 1, cur.1)) {
+    if cur.0 > 0 {
         neighbours.push((cur.0 - 1, cur.1));
     }
-    if cur.1 > 0 && !visited.contains_key(&(cur.0, cur.1 - 1)) {
+    if cur.1 > 0 {
         neighbours.push((cur.0, cur.1 - 1));
     }
-    if cur.0 < max && !visited.contains_key(&(cur.0 + 1, cur.1)) {
+    if cur.0 < max {
         neighbours.push((cur.0 + 1, cur.1));
     }
-    if cur.1 < max && !visited.contains_key(&(cur.0, cur.1 + 1)) {
+    if cur.1 < max {
         neighbours.push((cur.0, cur.1 + 1));
     }
 
